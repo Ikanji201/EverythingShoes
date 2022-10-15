49db6f7e8f917343106c992a07fae441129b6bbe
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-10-15 14:05:27 +0400

    cleanup, fixes and improvement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,41 @@
-import React from "react"
- 
+import React from "react";
 
 const Navbar = (props) => {
-  return (
-    <header>
+	return (
+		<header>
+			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-light">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="/#">
+						EVERYTHINGSHOES
+					</a>
 
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-light">
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarCollapse"
+						aria-controls="navbarCollapse"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon" />
+					</button>
 
-      <div className="container-fluid">
-
-        <a className="navbar-brand" href="/#">EVERYTHINGSHOES</a>
-
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-
-          <span className="navbar-toggler-icon" />
-
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-
-          
-
-            <button className="btn btn-outline-primary jed" type="submit">Balance: {props.balance} </button>
-
-        </div>
-
-      </div>
-
-    </nav>
-
-  </header>
-  );
+					<div
+						className="collapse navbar-collapse"
+						id="navbarCollapse"
+					>
+						<button
+							className="btn btn-outline-primary jed"
+							type="submit"
+						>
+							Balance: {props.balance}{" "}
+						</button>
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
 };
 
-export default Navbar
+export default Navbar;
