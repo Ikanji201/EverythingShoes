520e2732cc840e8263b4fa242d008295df360e18
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-15 07:35:17 +0530

    made improvements

--- src/contract/Product.sol
+++ src/contract/Product.sol
@@ -1,4 +1,5 @@
  // SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 pragma solidity >=0.7.0 <0.9.0;
 
@@ -28,9 +29,9 @@ interface IERC20Token {
 }
 
 contract Marketplace {
-
-    uint internal shoesLength = 0;
-    address internal cUsdTokenAddress =  0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    using Counters for Counters.Counter;
+    Counters.Counter shoesLength;
+    address internal constant cUsdTokenAddress =  0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     
     struct Product {
@@ -40,42 +41,53 @@ contract Marketplace {
         string size;
         uint price;
         uint sold;
-
     }
-    	mapping (uint => Product) internal products;
-
-        //Modifier that allows only the owner of the product  to access the functions
-    modifier onlyOwner(uint _index){
-        require(msg.sender == products[_index].owner, "Only the owner can access this function");
-        _;
-    }
-
+    
+    mapping (uint => Product) internal products;
 
   function  addProduct(
         string memory _image, 
         string memory _brand,
-         string memory _size,
-        uint _price
-        
-        
-    ) public {
-         uint _sold = 0;
-
-         products [shoesLength] =  Product(
-            payable(msg.sender),
-            
+        string memory _size,
+        uint _price  
+        ) public 
+    {   
+         products [shoesLength.current()] =  Product(
+            payable(msg.sender),     
             _image,
             _brand,
             _size,
             _price,
-            _sold
-            
-             
+            0          
         );
-
-        shoesLength++;
+        shoesLength.increment();
     }
 
+
+    function buyProduct(uint _index) public payable  {
+        require(msg.sender != products[_index].owner, "owner can't access");
+		require(
+		  IERC20Token(cUsdTokenAddress).transferFrom(
+			msg.sender,
+			products[_index].owner,
+			products[_index].price
+		  ),
+		  "Transfer failed."
+		);
+		products[_index].sold++;
+	}
+
+    // to replace and old shoe with a new shoe
+    function update(uint _index, string memory _newImage, string memory _newBrand) public returns(string memory, string memory) {
+        require(msg.sender == products[_index].owner, "Only the owner can access this function");
+        Product memory updateShoe = products[_index];
+        updateShoe.image = _newImage;
+        updateShoe.brand = _newBrand;
+        // You can replace the old shoe at specific index to a new shoe.
+        // You can do it, using the statement declared below this line  
+        products[_index] = updateShoe;
+        return(updateShoe.image, updateShoe.brand);
+    }
 
     function readProduct(uint _index) public view returns (
 		address payable,
@@ -95,34 +107,11 @@ contract Marketplace {
 		);
 	}
 
-
-    function buyProduct(uint _index) public payable  {
-		require(
-		  IERC20Token(cUsdTokenAddress).transferFrom(
-			msg.sender,
-			products[_index].owner,
-			products[_index].price
-		  ),
-		  "Transfer failed."
-		);
-		products[_index].sold++;
-	}
-
 	function getProductsLength() public view returns (uint) {
-		return (shoesLength);
+		return (shoesLength.current());
 	}
 
 
-    // to replace and old shoe with a knew shoe
-    function update(uint _index, string memory _newImage, string memory _newBrand) public returns(string memory, string memory) {
-        Product memory updateShoe = products[_index];
-        updateShoe.image = _newImage;
-        updateShoe.brand = _newBrand;
-        // You can replace the old shoe at specific index to a new shoe.
-        // You can do it, using the statement declared below this line  
-        products[_index] = updateShoe;
-        return(updateShoe.image, updateShoe.brand);
-    }
 
 }
 
